f1595069d718c25d92dbdec92c94d375d5903767
Ben Zenker <imbenzenker@gmail.com>
2017-10-07 16:39:18 -0400

    bugfix for env vars

--- worker/database/dbRead.js
+++ worker/database/dbRead.js
@@ -1,13 +1,9 @@
 const DynamoDB = require('aws-sdk/clients/dynamodb')
 
-<<<<<<< HEAD
-require('../../env') // load environment variables
-=======
 if (process.env.NODE_ENV !== 'production') {
   // eslint-disable-next-line global-require
   require('../../env') // load environment variables
 }
->>>>>>> scraper
 
 
 // Database Connection
