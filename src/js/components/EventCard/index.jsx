275d3d1fa00d6c58cc4002302e5d0b3c211aaebe
Ben Zenker <imbenzenker@gmail.com>
2018-02-16 23:17:28 -0500

    import change

--- src/js/components/EventCard/index.jsx
+++ src/js/components/EventCard/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Card from '../Card'
+import { Card } from '../Card'
 import { IconLink } from '../Content'
 import { EventType, EventContent, EventName,
   EventTime, IconContainer } from './style'
