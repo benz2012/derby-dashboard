87a51050ab974461bb788fed43aaa14150a9e7c1
Ben Zenker <imbenzenker@gmail.com>
2019-02-09 15:07:04 -0500

    hid links and routes based on users permission group

--- server/routes/data/auth.js
+++ server/routes/data/auth.js
@@ -12,12 +12,11 @@ router.get('/', (req, res) => {
     TableName: 'Derby_App',
     Key: { DataName: 'Authorized' },
   }).then((data) => {
-    const authorized = Object.keys(data.AdminPanel).reduce((acc, key) => {
-      // eslint-disable-next-line no-param-reassign
-      acc = [...acc, ...data.AdminPanel[key]]
-      return acc
-    }, []).indexOf(uid) !== -1
-    return res.json({ authorized })
+    const groups = Object.keys(data.AdminPanel)
+      .filter(g => data.AdminPanel[g].indexOf(uid) !== -1)
+    const authorized = groups.length > 0
+    const group = authorized && groups[0]
+    return res.json({ authorized, group })
   }).catch(err => errorEnd(err, res))
 })
 
