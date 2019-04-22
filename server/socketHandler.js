2e1bcd7a158d0566700d042074b37b7b8d9e528d
Ben Zenker <imbenzenker@gmail.com>
2019-04-22 15:55:07 -0400

    retreiving client address from new socket api

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -3,22 +3,22 @@ const socketHandler = (io) => {
   const cheering = {}
 
   io.on('connection', (socket) => {
-    const { address } = socket.handshake
-    console.log(`connected ${address} with socket ${socket.id}`)
-    socket.emit('address', address) // send browser their IP string
+    const { remoteAddress } = socket.connection
+    console.log(`connected ${remoteAddress} with socket ${socket.id}`)
+    socket.emit('address', remoteAddress) // send browser their IP string
 
     // Keep track of the number of people disconnected, 'tab/id' agnostic
-    if (clients[address] === undefined) {
-      clients[address] = {}
+    if (clients[remoteAddress] === undefined) {
+      clients[remoteAddress] = {}
     }
-    clients[address][socket.id] = true
+    clients[remoteAddress][socket.id] = true
     console.log(JSON.stringify(clients))
     io.emit('watching', Object.keys(clients).length)
     io.emit('cheering', cheering)
 
     // Keep track of the teams that each person is cheering for
     socket.on('cheering', ({ cheeringFor }) => {
-      cheering[address] = cheeringFor
+      cheering[remoteAddress] = cheeringFor
       io.emit('cheering', cheering)
     })
 
@@ -27,12 +27,12 @@ const socketHandler = (io) => {
       console.log(`disconnected ${socket.id}`)
 
       // Remove their tracking reference
-      delete clients[address][socket.id]
-      if (Object.keys(clients[address]).length === 0) {
+      delete clients[remoteAddress][socket.id]
+      if (Object.keys(clients[remoteAddress]).length === 0) {
         // If this is the last socket (tab) per IP, remove them
         // completely and their cheering reference
-        delete clients[address]
-        delete cheering[address]
+        delete clients[remoteAddress]
+        delete cheering[remoteAddress]
       }
 
       // Send the updates to anyone still connected
