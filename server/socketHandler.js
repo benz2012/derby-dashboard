a948a64a72919effec5186acb03088b3d7fd39e3
Ben Zenker <imbenzenker@gmail.com>
2019-04-22 17:21:13 -0400

    attempting to access correct ip data

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -3,22 +3,22 @@ const socketHandler = (io) => {
   const cheering = {}
 
   io.on('connection', (socket) => {
-    const { remoteAddress } = socket.conn
-    console.log(`connected ${remoteAddress} with socket ${socket.id}`)
-    socket.emit('address', remoteAddress) // send browser their IP string
+    const ipAddress = socket.handshake.headers['x-real-ip']
+    console.log(`connected ${ipAddress} with socket ${socket.id}`)
+    socket.emit('address', ipAddress) // send browser their IP string
 
     // Keep track of the number of people disconnected, 'tab/id' agnostic
-    if (clients[remoteAddress] === undefined) {
-      clients[remoteAddress] = {}
+    if (clients[ipAddress] === undefined) {
+      clients[ipAddress] = {}
     }
-    clients[remoteAddress][socket.id] = true
+    clients[ipAddress][socket.id] = true
     console.log(JSON.stringify(clients))
     io.emit('watching', Object.keys(clients).length)
     io.emit('cheering', cheering)
 
     // Keep track of the teams that each person is cheering for
     socket.on('cheering', ({ cheeringFor }) => {
-      cheering[remoteAddress] = cheeringFor
+      cheering[ipAddress] = cheeringFor
       io.emit('cheering', cheering)
     })
 
@@ -27,12 +27,12 @@ const socketHandler = (io) => {
       console.log(`disconnected ${socket.id}`)
 
       // Remove their tracking reference
-      delete clients[remoteAddress][socket.id]
-      if (Object.keys(clients[remoteAddress]).length === 0) {
+      delete clients[ipAddress][socket.id]
+      if (Object.keys(clients[ipAddress]).length === 0) {
         // If this is the last socket (tab) per IP, remove them
         // completely and their cheering reference
-        delete clients[remoteAddress]
-        delete cheering[remoteAddress]
+        delete clients[ipAddress]
+        delete cheering[ipAddress]
       }
 
       // Send the updates to anyone still connected
